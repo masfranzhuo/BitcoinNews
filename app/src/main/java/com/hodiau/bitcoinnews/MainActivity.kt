87200c486d4196728ff894049be0fb15cbaa6b37
Masfran <masfranzhuo@gmail.com>
2017-12-13 11:10:17 +0700

    added hidden api key

--- app/src/main/java/com/hodiau/bitcoinnews/MainActivity.kt
+++ app/src/main/java/com/hodiau/bitcoinnews/MainActivity.kt
@@ -3,9 +3,10 @@ package com.hodiau.bitcoinnews
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.hodiau.startup.R
+import com.hodiau.startup.BuildConfig;
 
 class MainActivity : AppCompatActivity() {
-
+    var apiKey:String = BuildConfig.NEWSAPI_API_KEY
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
